cb66e7ca88a36b3e70ffc8a19f1b366e541cc910
thomasxd24 <tomas_tse24@me.com>
2015-03-09 22:07:26 +0100

    finished

---
+++ JJA/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WebKit;
+using WebKit.DOM;
+
+namespace JJA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            webKitBrowser1.Navigate(textBox1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            webKitBrowser1.StringByEvaluatingJavaScriptFromString("updateProduct('102550','4','/products/update',1,110,'divAlertQteDispoMessage',0,900,'divAlertNbRowsMessage');");
+            webKitBrowser1.StringByEvaluatingJavaScriptFromString("updateProduct('709663341','4','/products/update',1,110,'divAlertQteDispoMessage',0,900,'divAlertNbRowsMessage');");
+            webKitBrowser1.StringByEvaluatingJavaScriptFromString("updateProduct('700749152','4','/products/update',1,110,'divAlertQteDispoMessage',0,900,'divAlertNbRowsMessage');");
+        }
+    }
+}
